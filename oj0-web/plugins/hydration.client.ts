52c6c836fe0d294f649919ffa380039dbb79bedb
泠韵 <2138889191@qq.com>
2023-12-03 19:45:28 +0800

    chore: update

--- oj0-web/plugins/hydration.client.ts
+++ oj0-web/plugins/hydration.client.ts
@@ -1,5 +1,5 @@
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.hooks.hookOnce('app:suspense:resolve', () => {
+  nuxtApp.hooks.hookOnce('app:suspense:resolve', () => { // 在 app created 之前执行
     isHydrated.value = true
   })
 })
