69df946bcb0e828d06cde0812712634fd99fa78d
泠韵 <2138889191@qq.com>
2023-12-04 22:02:05 +0800

    chore: update

--- oj0-web/components/nav/NavListMenu.vue
+++ oj0-web/components/nav/NavListMenu.vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
 const route = useRoute()
-const router = useRouter()
 
 const menus = getAppMenus()
 const themeMode = computed(() => isDark.value ? 'dark' : 'light')
 
-const selectedKeys = computed(() => {
-  return menus.value.filter(i => i.path === route.path)
-    .map(i => i.path)
-})
+const defaultSelectedKeys = ref<string[]>([route.path])
 </script>
 
 <template>
   <div h-full w-full>
-    <a-menu mode="horizontal" :selected-keys="selectedKeys" :theme="themeMode" :style="{ backgroundColor: 'transparent' }">
+    <a-menu mode="horizontal" :default-selected-keys="defaultSelectedKeys" :theme="themeMode" :style="{ backgroundColor: 'transparent' }">
       <a-menu-item v-for="menu in menus" :key="menu.path">
         <RouterLink :to="menu.path">
           {{ menu.name }}
