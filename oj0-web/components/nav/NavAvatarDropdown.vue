a86869298bfe1bb356acc7caa53c6d831bd1fa76
cosmoscatts <2138889191@qq.com>
2023-12-05 11:21:37 +0800

    chore: nav avatar

--- oj0-web/components/nav/NavAvatarDropdown.vue
+++ oj0-web/components/nav/NavAvatarDropdown.vue
@@ -6,8 +6,34 @@
   <a-dropdown trigger="click">
     <NavAvatar cursor-pointer :size="28" />
     <template #content>
-      <a-doption>个人中心</a-doption>
-      <a-doption>退出登陆</a-doption>
+      <div w-250px p-4>
+        <div h-50px flex-y-center shrink-0 gap-3>
+          <NavAvatar />
+
+          <div pl-3>
+            <div text-xl font-bold>
+              赤徑
+            </div>
+            <div mt-1 text-primary>
+              I have no bio.
+            </div>
+          </div>
+        </div>
+        <div mt-4>
+          <a-doption>
+            <div flex-y-center gap-4>
+              <div i-ri-profile-line />
+              个人中心
+            </div>
+          </a-doption>
+          <a-doption>
+            <div flex-y-center gap-4>
+              <div i-ri-logout-box-r-line />
+              退出登陆
+            </div>
+          </a-doption>
+        </div>
+      </div>
     </template>
   </a-dropdown>
 </template>
