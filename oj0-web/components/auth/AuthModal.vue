c3824056c1a44e1fa9568d7454fb64eeef3a6666
cosmoscatts <2138889191@qq.com>
2023-12-05 14:53:45 +0800

    chore: update

---
+++ oj0-web/components/auth/AuthModal.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const visible = defineModel()
+</script>
+
+<template>
+  <a-modal v-model:visible="visible" :footer="false" :closable="false">
+    <div h-500px>
+      Auth
+    </div>
+  </a-modal>
+</template>
--- oj0-web/components/nav/NavLoginBtns.vue
+++ oj0-web/components/nav/NavLoginBtns.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
+const modalVisible = ref(false)
 
+function register() {
+  modalVisible.value = true
+}
+
+function login() {
+  modalVisible.value = true
+}
 </script>
 
 <template>
   <div h-full flex-center gap-3>
-    <a cursor-pointer op-75 hover:op-100>注册</a>
+    <a cursor-pointer op-75 hover:op-100 @click="register">注册</a>
     <div>或</div>
-    <a cursor-pointer op-75 hover:op-100>登录</a>
+    <a cursor-pointer op-75 hover:op-100 @click="login">登录</a>
+
+    <AuthModal v-model="modalVisible" />
   </div>
 </template>
