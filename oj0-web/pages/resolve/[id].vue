ce47052316f61c87d183f3bb3810b32833a0a604
泠韵 <2138889191@qq.com>
2023-12-11 19:14:34 +0800

    chore: update

--- oj0-web/pages/resolve/[id].vue
+++ oj0-web/pages/resolve/[id].vue
@@ -17,8 +17,36 @@ const id = computed(() => route.params.id) // 题目 id
   <div h-screen w-screen>
     <ResolveActionBar />
 
-    <div px-30px>
-      做题 {{ id }}
+    <div :style="{ height: `calc(100vh - 6rem)` }" mb-2 of-hidden px-30px>
+      <a-split
+        :style="{
+          height: '100%',
+          width: '100%',
+          minWidth: '500px',
+        }" flex
+      >
+        <template #first>
+          <div h-full w-full of-x-hidden of-y-auto border="1 base">
+            1
+          </div>
+        </template>
+        <template #second>
+          <div>
+            <a-split direction="vertical" :style="{ height: 'calc(100vh - 6rem)', overflow: 'hidden' }">
+              <template #first>
+                <div h-full w-full of-x-hidden of-y-auto border="1 base">
+                  2
+                </div>
+              </template>
+              <template #second>
+                <div h-full w-full of-x-hidden of-y-auto border="1 base">
+                  3
+                </div>
+              </template>
+            </a-split>
+          </div>
+        </template>
+      </a-split>
     </div>
   </div>
 </template>
