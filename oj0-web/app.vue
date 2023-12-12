f3afe815e6bfb0a3a3666f70c038c9cd5b549dce
泠韵 <2138889191@qq.com>
2023-12-12 22:23:16 +0800

    chore: update

--- oj0-web/app.vue
+++ oj0-web/app.vue
@@ -1,11 +1,6 @@
 <script setup lang="ts">
 const authStore = useAuthStore()
 
-onMounted(async () => {
-  const result = await AuthApi.getLoginUser()
-  authStore.updateUser(result.data)
-})
-
 function createArcoTheme() {
   if (isDark.value)
     document.body.setAttribute('arco-theme', 'dark')
@@ -15,13 +10,24 @@ function createArcoTheme() {
 watch(isDark, () => {
   createArcoTheme()
   setThemeColor()
-}, { immediate: true })
+})
 
 const route = useRoute()
 watch(() => route.path, () => {
   const title = (route.meta.title ?? 'OJ0') as string
   useHead({ title })
 }, { immediate: true })
+
+async function checkLogin() {
+  const result = await AuthApi.getLoginUser()
+  authStore.updateUser(result.data)
+}
+
+onMounted(() => {
+  createArcoTheme()
+  setThemeColor()
+  checkLogin()
+})
 </script>
 
 <template>
