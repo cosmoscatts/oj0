5aefe06e9c3ee7e5f28cf9e82c9d9b1344726028
泠韵 <2138889191@qq.com>
2023-12-13 21:24:27 +0800

    chore: update

--- oj0-web/error.vue
+++ oj0-web/error.vue
@@ -7,10 +7,12 @@ const handleError = () => clearError({ redirect: '/' })
 </script>
 
 <template>
-  <div>
-    <h2>{{ error?.statusCode }}</h2>
-    <button @click="handleError">
-      Clear errors
-    </button>
+  <div min-h-screen flex="col center" text-base>
+    <div flex="~ col gap-2" items-center text-center>
+      <div i-ri-error-warning-line text-5xl /><span text-xl>发生了一些错误···</span>
+      <button mt-5 btn-solid @click="handleError">
+        清除错误
+      </button>
+    </div>
   </div>
 </template>
--- oj0-web/pages/no-auth.vue
+++ oj0-web/pages/no-auth.vue
@@ -6,10 +6,18 @@ definePageMeta({
   access: ACCESS_ENUM.NOT_LOGIN,
   middleware: 'auth',
 })
+
+const authStore = useAuthStore()
+const hasLogin = authStore.getHasLogin()
 </script>
 
 <template>
-  <div>
-    NoAuth
+  <div :style="{ height: 'calc(100vh - 150px)' }" min-h-300px min-w-200px w-full flex-center text-base>
+    <div v-if="!hasLogin" flex="~ col gap-2" items-center text-center>
+      <div i-ri-compasses-line text-5xl /><span text-xl>请先登录OJ0</span>
+    </div>
+    <div v-else flex="~ col gap-2" items-center text-center>
+      <div i-ri-forbid-line text-5xl /><span text-xl>您没有权限访问</span>
+    </div>
   </div>
 </template>
