5e7c75e855b4abbfb0675efcffb7ecc630df3519
泠韵 <2138889191@qq.com>
2023-12-14 20:55:16 +0800

    chore: update

---
+++ oj0-backend/src/main/java/com/cc/oj0backend/judge/JudgeManager.java
@@ -0,0 +1,7 @@
+package com.cc.oj0backend.judge;
+
+/**
+ * 判题管理
+ */
+public class JudgeManager {
+}
---
+++ oj0-backend/src/main/java/com/cc/oj0backend/judge/codesandbox/CodeSandbox.java
@@ -0,0 +1,16 @@
+package com.cc.oj0backend.judge.codesandbox;
+
+/**
+ * 代码沙箱接口定义
+ */
+public interface CodeSandbox {
+
+    /**
+     * 执行代码
+     *
+     * @param executeCodeRequest
+     * @return
+     */
+//    ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest);
+
+}
