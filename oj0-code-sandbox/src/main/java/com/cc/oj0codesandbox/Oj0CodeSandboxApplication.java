c2960e01983bdf9a5b224010047a75c4d1a6161a
泠韵 <2138889191@qq.com>
2023-12-22 21:10:26 +0800

    chore: init code sandbox

---
+++ oj0-code-sandbox/src/main/java/com/cc/oj0codesandbox/Oj0CodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.cc.oj0codesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oj0CodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oj0CodeSandboxApplication.class, args);
+    }
+
+}
---
+++ oj0-code-sandbox/src/test/java/com/cc/oj0codesandbox/Oj0CodeSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.oj0codesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oj0CodeSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
