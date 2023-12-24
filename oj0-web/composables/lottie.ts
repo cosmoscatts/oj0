0f268e50f319e481cd5a2abb0bbc39d6f1c1bf5f
泠韵 <2138889191@qq.com>
2023-12-24 10:03:13 +0800

    chore: update

---
+++ oj0-web/composables/lottie.ts
@@ -0,0 +1,27 @@
+import lottie from 'lottie-web'
+import type { RendererType } from 'lottie-web'
+
+export interface LottieParams {
+  containerId: string
+  path: string
+  loop?: boolean
+  renderer?: RendererType
+}
+
+const getElement = (id: string) => document.querySelector(id)!
+
+export function useLottie({
+  containerId,
+  path,
+  loop = true,
+  renderer = 'svg',
+}: LottieParams) {
+  return lottie.loadAnimation({
+    path,
+    loop,
+    renderer,
+    container: getElement(containerId),
+  })
+}
+
+export const useListLottie = (list: LottieParams[]) => list.forEach(item => useLottie(item))
