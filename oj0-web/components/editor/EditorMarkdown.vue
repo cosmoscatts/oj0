da4cd9ccc84ca15b0c61261fa3865c6be5176cea
cosmoscatts <2138889191@qq.com>
2023-12-25 14:15:42 +0800

    chore: add more bytemd plugins

--- oj0-web/components/editor/EditorMarkdown.vue
+++ oj0-web/components/editor/EditorMarkdown.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import gfm from '@bytemd/plugin-gfm'
+import gemoji from '@bytemd/plugin-gemoji'
+import math from '@bytemd/plugin-math'
+import mermaid from '@bytemd/plugin-mermaid'
 import { Editor } from '@bytemd/vue-next'
 import zhHans from 'bytemd/locales/zh_Hans.json'
 import highlightjs from 'highlight.js'
@@ -16,7 +19,12 @@ onMounted(() => {
 /**
  * 定义编辑器需要用到的插件
  */
-const plugins = [gfm()]
+const plugins = [
+  gfm(),
+  gemoji(),
+  math(),
+  mermaid(),
+]
 
 function handleChange(newValue: string) {
   value.value = newValue
--- oj0-web/components/editor/ViewerMarkdown.vue
+++ oj0-web/components/editor/ViewerMarkdown.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import gfm from '@bytemd/plugin-gfm'
+import gemoji from '@bytemd/plugin-gemoji'
+import math from '@bytemd/plugin-math'
+import mermaid from '@bytemd/plugin-mermaid'
 import { Viewer } from '@bytemd/vue-next'
 import highlightjs from 'highlight.js'
 
@@ -14,7 +17,12 @@ onMounted(() => {
 /**
  * 定义编辑器需要用到的插件
  */
-const plugins = [gfm()]
+const plugins = [
+  gfm(),
+  gemoji(),
+  math(),
+  mermaid(),
+]
 
 const themeClass = computed(() => isDark.value ? 'hljs-atom-one-dark' : 'hljs-atom-one-light')
 </script>
