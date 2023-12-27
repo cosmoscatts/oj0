ed6d5fd3ce4d51799a25f6832ccd338f0a5813fb
cosmoscatts <2138889191@qq.com>
2023-12-27 09:43:22 +0800

    chore: update

--- oj0-web/error.vue
+++ oj0-web/error.vue
@@ -9,9 +9,14 @@ const handleError = () => clearError({ redirect: '/' })
 <template>
   <div min-h-screen flex="col center" text-base>
     <div flex="~ col gap-2" items-center text-center>
-      <div i-ri-error-warning-line text-5xl /><span text-xl>发生了一些错误···</span>
+      <template v-if="error?.statusCode === 404">
+        <span mb-10 text-4xl>⊙﹏⊙∥ 页面未找到</span>
+      </template>
+      <template v-else>
+        <div i-ri-error-warning-line text-5xl /><span text-xl>发生了一些错误···</span>
+      </template>
       <button mt-5 btn-solid @click="handleError">
-        清除错误
+        {{ error?.statusCode === 404 ? '去首页看看吧~' : '清除错误' }}
       </button>
     </div>
   </div>
