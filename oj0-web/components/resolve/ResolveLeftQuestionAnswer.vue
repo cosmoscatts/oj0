4a8b548878aa4393e088edb7c54691e74c2ddff8
cosmoscatts <2138889191@qq.com>
2024-02-20 09:35:12 +0800

    chore: update

--- oj0-web/components/resolve/ResolveLeftQuestionAnswer.vue
+++ oj0-web/components/resolve/ResolveLeftQuestionAnswer.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import empty from '~/public/empty.svg'
+import empty from '~/public/empty.png'
 
 const { answer = '' } = defineProps<{
   answer?: string
@@ -12,8 +12,8 @@ const hasAnswer = computed(() => answer !== '')
   <div h-full of-y-auto px-3 py-10px>
     <ViewerMarkdown v-if="hasAnswer" :value="answer" />
     <div v-else flex="center col" h-full min-h-420px min-w-300px py-50px>
-      <div h-300px flex-center>
-        <img :src="empty" h-300px select-none>
+      <div h-180px flex-center>
+        <img :src="empty" w-200px select-none>
       </div>
       <div mt-5 text-center text-xl>
         Sorry, 题解还未发布
